7bec9ca20fd936721e94a413d83720f55e3a3d20
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-03 14:19:43 +0000

    bootstrap入れかえ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require rails-ujs
+//= require jquery_ujs
+//= require twitter/bootstrap
 //= require_tree .
