b8daf530b844d2555d38bc1fe6a3cffcef80020a
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-03 11:34:37 +0000

    買い物のインプットを整える

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_cart
+
+  def current_cart
+    if session[:cart_id]
+      @cart ||= Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create(order_status_id: 1)
+      session[:cart_id] = @cart.id
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :products
+  resources :carts, only: [:show] do
+    member do
+      post :add_item
+    end
+  end
 end
