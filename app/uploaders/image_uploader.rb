e65fa397944345e956360892174506d2d61ead1b
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-03 06:39:26 +0000

    carreiarwave導入

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,31 @@
+class ImageUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::RMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_to_fill: [200, 200]
+  process convert: 'jpg'
+
+  # Create different versions of your uploaded files:
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename
+  end
+end
